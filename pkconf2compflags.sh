c6d74990a7fb1fa8475d182ef990e6854238359e
Tony Angello <taaffaat_12@hotmail.com>
2022-05-07 13:04:19 -0500

    moved to scripts folder

--- pkconf2compflags.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-CFLAGS=`pkg-config $1 --cflags`
-
-for CFLAG in $CFLAGS
-do
-    echo $CFLAG >> $2/compile_flags.txt
-done
--- youtube_playlist.js
+++
@@ -1,9 +0,0 @@
-var playlist = document.getElementsByClassName("yt-simple-endpoint style-scope ytd-playlist-panel-video-renderer");
-
-for (let i = 0; i < playlist.length; ++i)
-{
-    var video_list = playlist[i].href;
-    var index_list = video_list.indexOf("&");
-    var only_video = video_list.substr(0, index_list);
-    console.log(only_video);
-}
